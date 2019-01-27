a89eb6d4765b0f8c65a71c003dadb216ea060302
Joost van der Schee <jhvanderschee@gmail.com>
2019-01-27 21:25:22 +0100

    update

--- pages/projects/add().php
+++ pages/projects/add().php
@@ -3,12 +3,19 @@ $customers = DB::selectPairs('select `id`,`name` from `customers` WHERE `tenant_
 if ($_SERVER['REQUEST_METHOD']=='POST') {
 	$data = $_POST;
 	
-	if (!isset($customers[$data['projects']['customer_id']])) $errors['projects[customer_id]']='Customer not found';
+	if ($data['projects']['add_customer']) {
+		$customer_id = DB::insert('INSERT INTO `customers` (`tenant_id`, `name`) VALUES (?, ?)', $_SESSION['user']['tenant_id'], $data['projects']['add_customer']);
+		$customers = DB::selectPairs('select `id`,`name` from `customers`  WHERE `tenant_id` = ?', $_SESSION['user']['tenant_id']);
+	} else {
+		$customer_id = $data['projects']['customer_id'];
+	}
+
+	if (!isset($customers[$customer_id])) $errors['projects[customer_id]']='Customer not found';
 	if (!isset($data['projects']['active']) || !$data['projects']['active']) $data['projects']['active'] = NULL;
 
 	if (!isset($errors)) {
 		try {
-			$id = DB::insert('INSERT INTO `projects` (`tenant_id`, `name`, `customer_id`, `active`) VALUES (?, ?, ?, ?)', $_SESSION['user']['tenant_id'], $data['projects']['name'], $data['projects']['customer_id'], $data['projects']['active']);
+			$id = DB::insert('INSERT INTO `projects` (`tenant_id`, `name`, `customer_id`, `active`) VALUES (?, ?, ?, ?)', $_SESSION['user']['tenant_id'], $data['projects']['name'], $customer_id, $data['projects']['active']);
 			if ($id) {
 				Flash::set('success','Project saved');
 				Router::redirect('projects/index');
