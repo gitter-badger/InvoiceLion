cc4914908c38ec44cc18f2b3e72b50500998eb92
Joost van der Schee <jhvanderschee@gmail.com>
2019-01-27 20:48:20 +0100

    update

--- pages/hours/add().php
+++ pages/hours/add().php
@@ -5,6 +5,13 @@ $hourtypes = DB::selectPairs('select `id`,`name` from `hourtypes` WHERE `tenant_
 if ($_SERVER['REQUEST_METHOD']=='POST') {
 	$data = $_POST;
 
+	if ($data['hours']['add_customer']) {
+		$customer_id = DB::insert('INSERT INTO `customers` (`tenant_id`, `name`) VALUES (?, ?)', $_SESSION['user']['tenant_id'], $data['hours']['add_customer']);
+		$customers = DB::selectPairs('select `id`,`name` from `customers`  WHERE `tenant_id` = ?', $_SESSION['user']['tenant_id']);
+	} else {
+		$customer_id = $data['hours']['customer_id'];
+	}
+
 	if (!isset($data['hours']['project_id']) || !$data['hours']['project_id']) $data['hours']['project_id']=NULL;
 	if (!isset($data['hours']['comment']) || $data['hours']['comment']) $data['hours']['comment']=NULL;
 	if (!isset($data['hours']['type']) || !$data['hours']['type']) $data['hours']['type']=NULL;
@@ -12,7 +19,7 @@ if ($_SERVER['REQUEST_METHOD']=='POST') {
 	if (!isset($data['hours']['customer_id']) || !$data['hours']['customer_id']) $errors['hours[customer_id]']='Customer not set';	
 	
 	//set vat_percentage to NULL if the customer has vat_reverse_charge
-	$vat_reverse_charge = DB::selectValue('select `vat_reverse_charge` from `customers` WHERE `tenant_id` = ? AND `id` = ?', $_SESSION['user']['tenant_id'], $data['hours']['customer_id']);
+	$vat_reverse_charge = DB::selectValue('select `vat_reverse_charge` from `customers` WHERE `tenant_id` = ? AND `id` = ?', $_SESSION['user']['tenant_id'], $customer_id);
 	if ($vat_reverse_charge) $data['hours']['vat_percentage']=NULL;
 	
 	if (!isset($errors)) {
@@ -31,7 +38,7 @@ if ($_SERVER['REQUEST_METHOD']=='POST') {
 				`total`
 			) VALUES (?, ?, ?, ?, ?, ?, ?)', 
 				$_SESSION['user']['tenant_id'], 
-				$data['hours']['customer_id'], 
+				$customer_id, 
 				$data['hours']['name'], 
 				$subtotal, 
 				($total - $subtotal),
@@ -54,7 +61,7 @@ if ($_SERVER['REQUEST_METHOD']=='POST') {
 				`invoiceline_id`
 			) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', 
 				$_SESSION['user']['tenant_id'],
-				$data['hours']['customer_id'], 
+				$customer_id, 
 				$data['hours']['project_id'], 
 				$data['hours']['date'], 
 				$data['hours']['name'], 
